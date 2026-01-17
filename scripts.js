d2e0b2933493fe2352d8a8d5fab1084713d81b91
Fedor Trishkin <fedor.trishkin@gmail.com>
2026-01-17 16:22:28 +0400

    Small updates
    Removed hard coded initial value and updated the function using arguments and parameters.

--- scripts.js
+++ scripts.js
@@ -4,31 +4,46 @@ const pMass = document.getElementById("p-mass");
 const inputEl = document.getElementById("input-el");
 const btn = document.getElementById("btn-el");
 
+function render(userValue) {
+  const calcFeet = (userValue * 3.281).toFixed(3);
+  const calcMeters = (userValue / 3.281).toFixed(3);
+  const calcLiters = (userValue * 3.785).toFixed(3);
+  const calcGallons = (userValue / 3.785).toFixed(3);
+  const calcPounds = (userValue * 2.205).toFixed(3);
+  const calcKilos = (userValue / 2.205).toFixed(3);
+  // Text template: 20 meters = 65.616 feet | 20 feet = 6.096 meters
+  if (userValue === 1) {
+    pLength.textContent = `${userValue} meter = ${calcFeet} feet | ${userValue} foot = ${calcMeters} meters`;
+  } else {
+    pLength.textContent = `${userValue} meters = ${calcFeet} feet | ${userValue} feet = ${calcMeters} meters`;
+  }
+  // Text template: 20 liters = 5.284 gallons | 20 gallons = 75.708 liters
+  if (userValue === 1) {
+    pVolume.textContent = `${userValue} liter = ${calcGallons} gallons | ${userValue} gallon = ${calcLiters} liters`;
+  } else {
+    pVolume.textContent = `${userValue} liters = ${calcGallons} gallons | ${userValue} gallons = ${calcLiters} liters`;
+  }
+  // Text template: 20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
+  if (userValue === 1) {
+    pMass.textContent = `${userValue} kilo = ${calcPounds} pounds | ${userValue} pound = ${calcKilos} kilos`;
+  } else {
+    pMass.textContent = `${userValue} kilos = ${calcPounds} pounds | ${userValue} pounds = ${calcKilos} kilos`;
+  }
+}
+
 btn.addEventListener("click", function () {
-  const myValue = inputEl.value;
-  const numberValue = Number(myValue);
-  const calcFeet = (numberValue * 3.281).toFixed(3);
-  const calcMeters = (numberValue / 3.281).toFixed(3);
-  const calcLiters = (numberValue * 3.785).toFixed(3);
-  const calcGallons = (numberValue / 3.785).toFixed(3);
-  const calcPounds = (numberValue * 2.205).toFixed(3);
-  const calcKilos = (numberValue / 2.205).toFixed(3);
-  // 20 meters = 65.616 feet | 20 feet = 6.096 meters
-  if (numberValue === 1) {
-    pLength.textContent = `${numberValue} meter = ${calcFeet} feet | ${numberValue} foot = ${calcMeters} meters`;
-  } else {
-    pLength.textContent = `${numberValue} meters = ${calcFeet} feet | ${numberValue} feet = ${calcMeters} meters`;
+  const userInput = inputEl.value;
+  const value = Number(userInput);
+
+  if (userInput === "") {
+    alert("Please enter a number");
+    return;
   }
-  // 20 liters = 5.284 gallons | 20 gallons = 75.708 liters
-  if (numberValue === 1) {
-    pVolume.textContent = `${numberValue} liter = ${calcGallons} gallons | ${numberValue} gallon = ${calcLiters} meters`;
-  } else {
-    pVolume.textContent = `${numberValue} liters = ${calcGallons} gallons | ${numberValue} gallons = ${calcLiters} meters`;
+
+  if (Number.isNaN(value)) {
+    alert("Invalid number");
+    return;
   }
-  //   20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
-  if (numberValue === 1) {
-    pMass.textContent = `${numberValue} kilo = ${calcPounds} pounds | ${numberValue} pound = ${calcKilos} kilos`;
-  } else {
-    pMass.textContent = `${numberValue} kilos = ${calcPounds} pounds | ${numberValue} pounds = ${calcKilos} kilos`;
-  }
+
+  render(value);
 });
